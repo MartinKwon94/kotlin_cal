4e064060b9c7b09ff78f16dc37d1cac98f3e8b41
Martin Kwon <mindori9097@gmail.com>
2023-07-18 19:57:47 +0900

    git commit -m "calculator"

--- app/src/main/java/com/example/myapplication/calculator.kt
+++ app/src/main/java/com/example/myapplication/calculator.kt
@@ -2,10 +2,6 @@ package com.example.myapplication
 class Calculator {
     fun addOperation(operation: AddOperation, operand1: Int, operand2: Int): Double {
         return operation.operate(operand1, operand2)
-        //위의 코드에서 'addOperation' 메서드는 'AddOperation' 클래스의 인스턴스인
-        //'operation'과 피연산자인 'operand1'과 'operand2'를 전달받아 덧셈 연산을 수행합니다.
-        //'operation.operate(operand1, operand2)'은 'AddOperation' 클래스의 'operate' 메서드를 호출하여 덧셈을 수행하고 그 결과를 반환합니다.
-        //이렇게 수정된 Calculator 클래스를 사용하여 주어진 연산자와 피연산자를 전달하여 덧셈 연산을 수행할 수 있습니다.
     }
 }
 fun main() {
